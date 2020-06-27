f945760edd1068cd82a94b9e1458482936ce197a
Lomig Enfroy <dunwael@gmail.com>
2020-06-27 17:50:35 +0200

    Fix code indentation

---
+++ CountOnMe/Models/Calculus.swift
@@ -0,0 +1,12 @@
+//
+//  Calculus.swift
+//  CountOnMe
+//
+//  Created by Lomig Enfroy on 27/06/2020.
+//  Copyright © 2020 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+
+class Calculus {
+}

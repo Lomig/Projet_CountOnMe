9278bb105da981cdd8fa6d9a07c045718975a3db
Lomig Enfroy <dunwael@gmail.com>
2020-06-28 19:10:55 +0200

    Improving Landscape Design

--- CountOnMe/Controllers/ViewController.swift
+++ CountOnMe/Controllers/ViewController.swift
@@ -25,7 +25,14 @@ class ViewController: UIViewController {
   }
 
   // View actions
-  @IBAction func tappedNumberButton(_ sender: UIButton) {
+  @IBAction func tappedNumberButton(_ sender: UIButton) { addNumberToExpression(sender) }
+  @IBAction func tappedAdditionButton(_ sender: UIButton) { addOperatorToExpression("+") }
+  @IBAction func tappedSubstractionButton(_ sender: UIButton) { addOperatorToExpression("-") }
+  @IBAction func tappedMultiplicationButton(_ sender: UIButton) { addOperatorToExpression("x") }
+  @IBAction func tappedDivisionButton(_ sender: UIButton) { addOperatorToExpression("÷") }
+  @IBAction func tappedEqualButton(_ sender: UIButton) { expression.evaluate(onSuccess: updateView, onFailure: showErrorMessage(_:)) }
+
+  private func addNumberToExpression(_ sender: UIButton) {
     guard let numberText = sender.title(for: .normal) else {
       return
     }
@@ -36,27 +43,7 @@ class ViewController: UIViewController {
     expression.add(numberText, onCompletion: updateView)
   }
 
-  @IBAction func tappedAdditionButton(_ sender: UIButton) {
-    addOperatorToExpression("+")
-  }
-
-  @IBAction func tappedSubstractionButton(_ sender: UIButton) {
-    addOperatorToExpression("-")
-  }
-
-  @IBAction func tappedMultiplicationButton(_ sender: UIButton) {
-    addOperatorToExpression("x")
-  }
-
-  @IBAction func tappedDivisionButton(_ sender: UIButton) {
-    addOperatorToExpression("÷")
-  }
-
-  @IBAction func tappedEqualButton(_ sender: UIButton) {
-    expression.evaluate(onSuccess: updateView, onFailure: showErrorMessage(_:))
-  }
-
-  func addOperatorToExpression(_ operationSymbol: String) {
+  private func addOperatorToExpression(_ operationSymbol: String) {
     if expression.hasResult {
       expression = Expression(expression.result)
     }
@@ -68,16 +55,16 @@ class ViewController: UIViewController {
     }
   }
 
-  func startNewExpression() {
+  private func startNewExpression() {
     textView.text = ""
     expression = Expression()
   }
 
-  func updateView() {
+  private func updateView() {
     textView.text = expression.literal
   }
 
-  func showErrorMessage(_ errorMessage: String) {
+  private func showErrorMessage(_ errorMessage: String) {
     let alertVC = UIAlertController(title: "Zéro!", message: errorMessage, preferredStyle: .alert)
     alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
     return self.present(alertVC, animated: true, completion: nil)
